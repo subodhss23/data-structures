3600288641e5bb7474f43102a67e36e4a16875c7
subodh <subodhss23@gmail.com>
2019-09-16 02:49:17 -0700

    what is an array?

---
+++ array.js
@@ -0,0 +1,10 @@
+// adding element on an aray
+
+function addition(newElement){
+    let arr = [1,2,3,4,5];
+    arr.push(newElement);
+    console.log(newElement);
+    console.log(arr);
+}
+
+addition(6);
