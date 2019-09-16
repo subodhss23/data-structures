328a873fc015d16317ecfd39eb039fcc6d077b0a
subodh <subodhss23@gmail.com>
2019-09-16 02:54:14 -0700

    get it

--- array.js
+++ array.js
@@ -7,4 +7,15 @@ function addition(newElement){
     console.log(arr);
 }
 
+function additionElement(newElement){
+    let arr = [2,3,4,5,6,7];
+    while (arr !== null){
+        arr = arr[arr.length + 1] + newElement;
+        console.log(`This is inside the loop ${arr}`);
+        break;
+    }
+    console.log(`now we are outside the loop ${arr}`);
+}
+
 addition(6);
+additionElement(66);
