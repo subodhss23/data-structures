181c6c843b00701d1e7809e073d32158f832062c
subodh <subodhss23@gmail.com>
2019-09-18 23:53:18 -0700

    stack with build in methods

--- array.js
+++ array.js
@@ -33,20 +33,69 @@
 // console.log(newPerson);
 
 
-const newObject = new Object();
-newObject.name = 'Subodh';
-newObject.age = 15;
-console.log(newObject);
-console.log(newObject.name);
-console.log(newObject.age);
-console.log('---------------------------')
+// const newObject = new Object();
+// newObject.name = 'Subodh';
+// newObject.age = 15;
+// console.log(newObject);
+// console.log(newObject.name);
+// console.log(newObject.age);
+// console.log('---------------------------')
 
-function Person(name, age){
-    this.name = name;
-    this.age = age;
+// function Person(name, age){
+//     this.name = name;
+//     this.age = age;
+// }
+
+// const newObejct = new Person('Subodh', 15);
+// console.log(newObject);
+// console.log(newObject.name);
+// console.log(newObject.age);
+
+// const person = {
+//     name: "Subodh",
+//     age: '27',
+//     address: "Hayward"
+// }
+
+// console.log(person['name']);
+// console.log(person['age']);
+
+
+// let arr = [1,2,3,3,4,5];
+// for (let i = 0; i <= arr[arr.length - 1]; i++){
+//     console.log(arr[i]);
+// }
+
+// let string = "thisisit";
+// for (let i = 0; i > string.length; i--){
+//     console.log(string[i]);
+// }
+
+//implementing simple stack
+
+class Stack{
+    constructor(arr, elem){
+        this.arr = arr;
+        this.elem = elem;
+    }
+
+    pushing(){
+         this.arr.push(this.elem);
+         return this.arr;
+    }
+
+    poping(){
+         return this.arr.pop()
+    }
+
+    print(){
+        return this.arr;
+    }
+
 }
 
-const newObejct = new Person('Subodh', 15);
-console.log(newObject);
-console.log(newObject.name);
-console.log(newObject.age);
+
+let newobj = new Stack([1,2,3,4], 5);
+console.log(newobj.pushing());
+console.log(newobj.poping());
+console.log(newobj.print())
