4874681f6d2eb024598c0c92d7c61f4798a2170f
subodh <subodhss23@gmail.com>
2019-09-18 22:52:33 -0700

    new beginning

--- array.js
+++ array.js
@@ -1,21 +1,35 @@
-// adding element on an aray
+// // adding element on an aray
 
-function addition(newElement){
-    let arr = [1,2,3,4,5];
-    arr.push(newElement);
-    console.log(newElement);
-    console.log(arr);
+// function addition(newElement){
+//     let arr = [1,2,3,4,5];
+//     arr.push(newElement);
+//     console.log(newElement);
+//     console.log(arr);
+// }
+
+// function additionElement(newElement){
+//     let arr = [2,3,4,5,6,7];
+//     while (arr !== null){
+//         arr = arr[arr.length-1] + newElement;
+//         console.log(arr);
+//         console.log(`This is inside the loop ${arr}`);
+//         break;
+//     }
+//     console.log(`now we are outside the loop ${arr}`);
+// }
+
+// // addition(6);
+// additionElement(66);
+
+
+function ConstructorFunction(lastName, firstName){
+    this.lastName = lastName;
+    this.firstName = firstName;
 }
 
-function additionElement(newElement){
-    let arr = [2,3,4,5,6,7];
-    while (arr !== null){
-        arr = arr[arr.length + 1] + newElement;
-        console.log(`This is inside the loop ${arr}`);
-        break;
-    }
-    console.log(`now we are outside the loop ${arr}`);
-}
+const newPerson = new ConstructorFunction('Sam', 'Smith');
+console.log(newPerson.lastName);
+console.log(newPerson.firstName);
+console.log(newPerson);
 
-addition(6);
-additionElement(66);
+
