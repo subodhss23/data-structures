cc409c514727e60e88572e39d10de722c9db6973
subodh <subodhss23@gmail.com>
2019-09-20 22:30:01 -0700

    practice

--- array.js
+++ array.js
@@ -1,1 +1,26 @@
-// clearing things down 
+// writing 100 lines of code at least
+//implementing transvering through all elements of a string 
+// let string = "iamastring";
+
+// for(let i = 0;i < string.length; --i){
+//     console.log(string[i]);
+// }
+
+// adding in element in certain position of a string
+// function addElement(elem, position){
+//     let string = "thisiselement";
+//     console.log(string[2]);
+
+// }
+
+let list = "what";
+var text = "";
+// for (var member in list){
+//     text = text + list[member]
+// }
+let member = 0;
+console.log(`This is what text looks before execution ----> ${text}`);
+text = text + list[member];
+console.log(`This is what text looks after execution  ----->  ${text}`);
+
+// addElement('a', 2);
