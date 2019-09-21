3c1761701869f514b20be5338fe554bc82cbdb38
subodh <subodhss23@gmail.com>
2019-09-21 10:09:26 -0700

    curious mind

--- array.js
+++ array.js
@@ -1,23 +1,29 @@
-// let's work on array now
+// push
+// pop
+// splice 
+// filter
 
-// let x = [1,2,3,4]
-// for (let i = 0; i < x.length; i++){
-//     console.log(x[i]);
-// }
 
-function insertionArray(position, element)
-{
-    let x = [1,2,3,4,5];
-    for (let i = 0; i < x.length; i++){
-        // if(x[position] !== null){
-        //     x[position] = element;
-        // }
-        while(true){
-            x[position] = element;
-            break;
-        }
-    }
-    console.log(x);
+// let arr = [1,2,3,4,5];
+// let string = "iamastring";
+
+// console.log(`this is very old array where no methods has been used ${arr}`);
+// let  newarr = arr.splice(0,2);
+// console.log(`This is new array ${newarr}`);
+// console.log(`This is our old array after splice has been implement on it${arr}`);
+
+// add an element to the array in certain position
+function addArray(element, position){
+    let array = [1,2,3,4];
+        array = array.splice(position, 1, element);
+    console.log(array);
 }
 
-insertionArray(2, 4444);
+addArray(9999, 4);
+
+// let newarr = arr.filter(function(element){
+//     return element < 4;
+// })
+
+// console.log(newarr);
+// console.log(arr);
