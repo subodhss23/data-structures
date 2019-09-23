271282f76a1f2baca0f799bc3f1e3d6d297cbc7e
subodh <subodhss23@gmail.com>
2019-09-23 15:43:52 -0700

    80%

---
+++ anotherDoublyLinkedList.js
@@ -0,0 +1,126 @@
+// implementation of doubly linked list
+
+class Node {
+    constructor(data){
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoublyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+    }
+
+    append(item){
+        let node = new Node(item);
+        if(!this.head){
+            this.head = node;
+            this.tail = node;
+        } else {
+            node.prev = this.tail;
+            this.tail.next = node;
+            this.tail = node;
+        }
+    }
+
+    appendAt(pos, item){
+        let current = this.head;
+        let current = 1;
+        let node= new Node(items);
+        if(pos === 0){
+            this.head.prev = node;
+            node.next = this.head;
+            this.head = node;
+        } else {
+            while (current){
+                current = current.next;
+                if (counter = pos){
+                    node.prev = current.prev;
+                    current.prev.next = node;
+                    node.next = current;
+                    current.prev = node;
+                }
+                counter++;
+            }
+        }  
+    }
+
+    // removing node
+    remove(item){
+        let current = this.head;
+        while(current){
+            if(current.data === item){
+                if(current == this.head && current == this.tail){
+                    this.head = null;
+                    this.tail = null;
+                } else if (current == this.head){
+                    this.head = this.head.next;
+                    this.head.prev = null;
+                } else if(current = this.tail){
+                    this.tail = this.tail.prev;
+                    this.tail.next = null;
+                } else {
+                    current.prev.next = current.next;
+                    current.next.prev = current.prev;
+                }
+        }
+        current = current.next;
+    }
+}
+
+removeAt(pos){
+    let current= this.head;
+    let counter = 1;
+    if(pos == 0){
+        this.head = this.head.next;
+        this.head.prev = null;
+    } else {
+        while(current){
+            current = current.next;
+            if(current == this.tail){
+                this.tail = this.tail.prev;
+                this.tail.next = null;
+            } else if (counter == pos){
+                current.prev.next = current.next;
+                current.next.prev = current.prev;
+                break;
+            }
+            counter++;
+        }
+    }
+}
+    reverse(){
+        let current = this.head;
+        let prev = null;
+        while(current){
+            let next = current.next;
+            current.next = prev;
+            current.prev = next;
+            prev = current;
+            current = next;
+        } 
+        this.tail = this.head;
+        this.head = prev;
+    }
+
+    swap(nodeOne, nodeTwo){
+        let current = this.head;
+        let counter = 0;
+        let firstNode;
+        while (current !== null){
+            if(counter == nodeOne){
+                fistNode = current;
+            } else if(counter == nodeTwo){
+                let temp = current.data;
+                current.data = firstNode.data;
+                firstNode.data = temp;
+            }
+            current = current.next;
+            counter++;
+        }
+        return true;
+    }
+}
