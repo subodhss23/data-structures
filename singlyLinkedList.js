a263f12022efc73c7adda38b1f79801d18689f0e
subodhss23 <subodhss23@gmail.com>
2019-10-05 13:34:17 -0700

    new linkedlist

---
+++ singlyLinkedList.js
@@ -0,0 +1,24 @@
+// what is node
+// stores piece of data - val
+// referemce to next node - next
+
+
+
+class Node{
+    constructor(val){
+     this.val = val;
+     this.next = null;   
+    }
+}
+
+class SinglyLinkedList{
+    constructor(){
+        this.length = 0;
+        this.head = null;
+        this.tail = null;
+    }
+
+    
+}
+
+
