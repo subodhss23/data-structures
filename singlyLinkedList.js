f2e22d9d9dcd6bd92a2e72d4506f2611a74a8a4a
subodhss23 <subodhss23@gmail.com>
2019-10-06 10:13:06 -0700

    poping

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -33,27 +33,60 @@ class SinglyLinkedList{
     //     }
     // }
 
+
+    /**poping pseudocode
+     * if no nodes, return undefined
+     * loop through the list until you reach the tail
+     * set the next property of the 2nd to last node to be null
+     * set the tail to be the 2nd to last node
+     * decrement the length of the list by 1
+     * return the value of the node removed
+     */
+
     pop(){
-        if(!this.head) return undefined;
-        var current = this.head;
-        var newTail = current;
-        while(current.next){
-                newTail = current;
-                current = current.next;
-            }
-            this.tail = newTail;
-            this.tail.next = null;
-            this.length--;
-            return current;
-        }
+     if(!this.head) return undefined;
+     var current = this.head;
+     var newTail = current;
+     while(current.next){
+        newTail = current;
+        current = current.next;
+     }
+     this.tail = newTail;
+     this.tail.next = null;
+     this.length--;
+     if(this.length === 0){
+         this.head = null;
+         this.tail = null;
+     }
+     return current;
     }
+
+    //shifting
+    // removing a new node from the beginning of the linked list
+
+    // shifting pseudocode
+    // if there in no nodes, return undefined
+    // store the current head property in a variable
+    // set the head property to be the current head's next property
+    // decrement the length by 1
+    // return the value of the node removed
+
+    shift(){
+        if(!this.head) return undefined;
+        var currentHead = this.head;
+        this.head = currentHead.next;
+        this.length--;
+        return currentHead;
+    }
+}
 
 var list = new SinglyLinkedList();
 list.push('hello');
 list.push('there');
 list.push('!');
 // console.log(list.traverse());
-list.pop();
+// list.pop();
+// console.log(list);
 console.log(list);
-
+ list.shift();
 // console.log(list);
