69d416a23338db0cb2dc51effd3459e061b57325
Subodh <subodhss23@gmail.com>
2019-10-12 19:01:34 -0700

    clear as crystal

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -1,33 +1,72 @@
-// setting up two classes
-// one node class
-// - value
-// - next
-// - prev
+// class Node{
+//     constructor(val){
+//         this.val = val;
+//         this.next = null;
+//         this.prev = null;
+//     }
+// }
 
-// doublylinkedlist class
-// -head
-// -tail
-// -length
+// class LinkedList{
+//     constructor(){
+//         this.head = null;
+//         this.tail = null;
+//         this.size = 0;
+//     }
+
+//     push(val){
+//         let newNode = new Node(val);
+//         if(!this.head || this.size === 0){
+//             this.head = newNode;
+//             this.tail = newNode;
+//         } else{
+//             this.tail.next = newNode;
+//             newNode.prev = this.tail;
+//             this.tail = newNode;
+//         }
+//         this.size++;
+//         return this;
+//     }
+// }
+
+// let list = new  LinkedList();
+// list.push(2);
+// list.push(3);
+// console.log(list);
+
+
+// what happens in single linked list then?
+
 
 class Node{
     constructor(val){
         this.val = val;
         this.next = null;
-        this.prev = null;
     }
 }
 
-class DoublyLinkedList {
+class SingleLinkedList{
     constructor(){
         this.head = null;
         this.tail = null;
-        this.length = 0;
+        this.size = 0;
     }
 
-    
+    push(val){
+        let newNode = new Node(val);
+        if(!this.head || this.size === 0){
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.size++;
+        return this;
+    }
 }
 
-let first = new Node(12);
-console.log(first);
-first.next = new Node(13);
-console.log(first.next);
+let list = new  SingleLinkedList();
+list.push(2);
+list.push(3);
+list.push(4);
+console.log(list);
