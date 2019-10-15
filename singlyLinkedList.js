4abd8faf0569144902c4a85ac78dc7be912fca02
Subodh <subodhss23@gmail.com>
2019-10-15 01:13:54 -0700

    basic adding and removing in front and at the back

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -30,15 +30,67 @@ class SinglyLinkedList{
 
     traverse(){
         let current = this.head;
-        while(current){
+        // while(current){
+        //     console.log(current.val);
+        //     current = current.next;
+        // }
+
+        for(let i = 0; i < this.length; i++){
             console.log(current.val);
             current = current.next;
         }
     }
+
+    //remove a node from the end of the linked list
+    pop(){
+        if(!this.head) return null;
+        let current = this.head;
+        let newTail = current;
+        
+        while(current.next){
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        newTail.next = null;
+        if(this.length === 0){
+            this.head = null;
+            this.tail = null;
+        }
+        this.length--;
+        console.log(current);
+    }
+
+    //remove the node from the beginning
+       shift(){
+           if(!this.head) return undefined;
+           let removingNode = this.head;
+           this.head = removingNode.next;
+           removingNode.next = null;
+           this.length--;
+           if(this.length === 0){
+               this.tail = null;
+           }
+           console.log(removingNode);
+       }
+
+    //adding a new node to the beginning of the linked list
+    unshift(val){
+        let newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+        } else{
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }       
 }
 
 let list = new SinglyLinkedList();
-list.push(2);
-list.push(3);
 list.push(4);
-console.log(list.traverse());
+
+list.shift();
+console.log(list);
