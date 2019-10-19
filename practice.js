f6c182b3635b8381bd92a5880602bf8a1123130f
Subodh <subodhss23@gmail.com>
2019-10-19 11:59:50 -0700

    power

--- practice.js
+++ practice.js
@@ -16,12 +16,35 @@
 // }
 // countDown(3);
 
-function factorial(num){
-    let total = 1;
-    for(let i = num; i > 1; i--){
-        total *= i;
-    }
-    return total;
-}
+// function factorial(num){
+//     let total = 1;
+//     for(let i = num; i > 1; i--){
+//         total *= i;
+//     }
+//     return total;
+// }
 
-console.log(factorial(10));
+// function collectionOddValues(arr){
+//     let newArr = [];
+
+//     if(arr.length === 0){
+//         newArr.push(arr[0]);
+//     }
+
+//     newArr = newArr.concat(collectionOddValues(arr.slice(1)));
+//     return newArr;
+// }
+
+// collectionOddValues([1,2,3,4,5]);
+
+
+// write a function called power which accepts a base and an exponent.
+
+function power(base, expo){
+   if(expo === 0) return 1;
+   else{
+       return base * power(base, expo-1);
+   }
+};
+
+console.log(power(2,2));
