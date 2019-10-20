c6911f311c2a2e75c6b9dcc7f00f51c5a5f2eea1
Subodh <subodhss23@gmail.com>
2019-10-20 13:34:04 -0700

    implementing search

--- BST.js
+++ BST.js
@@ -1,38 +1,73 @@
-// implementing bst 
-
 class Node{
-    constructor(value){
-        this.value = value;
-        this.left = null;
+    constructor(val){
+        this.val = val;
         this.right = null;
+        this.left = null;
     }
 }
 
-class BinarySearchTree{
+class BST{
     constructor(){
         this.root = null;
     }
 
-    insert(value){
-        let newNode = new Node(value);
+    insert(val){
+        let newNode = new Node(val);
         if(!this.root){
             this.root = newNode;
-            return this;
         } else {
             let current = this.root;
             while(true){
-                if(value < current.value){
+                console.log(current.val);
+                if(val === current.val) return undefined;
+                if(val < current.val){
                     if(current.left === null){
                         current.left = newNode;
                         return this;
                     } else {
-                        if(current.right === null){
-                            current.right = newNode;
+                        current = current.left;
+                    } 
+                } else if( val > current.val){
+                    if(current.right === null){
+                        current.right = newNode;
+                        return this;
+                    } else {
+                        current = current.right;
+                    }
+                }
+            }
+        }
+    }
+
+    find(val){
+        if(!this.root) return undefined;
+        if(this.root === this.val) return this;
+        else{
+                let current = this.head;
+                while(true){
+                    if(val < current.val){
+                        if(current.left === val){
                             return this;
+                        } else {
+                            current = current.left;
+                        } 
+                    } else  if( val > current.val){
+                        if(current.right === val){
+                            return this;
+                        } else {
+                            current = current.left;
                         }
                     }
                 }
             }
         }
     }
-}
+
+let newbst = new BST();
+newbst.insert(10);
+newbst.insert(5);
+newbst.insert(8);
+newbst.insert(12);
+newbst.insert(15);
+console.log(newbst);
+console.log(newbst.find(8));
